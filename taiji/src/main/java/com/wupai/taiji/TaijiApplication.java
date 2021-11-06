1dcbdbc316136b7c6e2d2b58cc9fa4df95268394
黑黑黑 <2270716426@qq.com>
2021-11-06 15:04:50 +0800

    测试4

--- taiji/src/main/java/com/wupai/taiji/TaijiApplication.java
+++ taiji/src/main/java/com/wupai/taiji/TaijiApplication.java
@@ -3,10 +3,13 @@ package com.wupai.taiji;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.rmi.ServerError;
+
 @SpringBootApplication
 public class TaijiApplication {
 
     public static void main(String[] args) {
+        System.err.println("ads");
         SpringApplication.run(TaijiApplication.class, args);
     }
 
